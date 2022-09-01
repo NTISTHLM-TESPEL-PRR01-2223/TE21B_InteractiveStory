4f6d7c9e03a64da45c817d06c9be9092897f3280
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-01 15:12:52 +0200

    Initial commit

---
+++ TE21B_InteractiveStory/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Läs in namnet
+Console.WriteLine("Hello, what is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}");
+
+// Kolla om namnet är Micke eller Karl
+if (name == "Micke")
+{
+  Console.WriteLine("Du skrev in rätt namn!");
+
+}
+else if (name == "Karl")
+{
+  Console.WriteLine("Wtf, Karl?");
+}
+else
+{
+  Console.WriteLine("Du skrev in fel namn!");
+}
+
+
+
+Console.ReadLine();
